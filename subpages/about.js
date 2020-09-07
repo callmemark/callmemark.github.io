fb43f5f61229be5409a8fbf1175a34bf4734c188
Mark John A. Velmonte <65215676+callmemark@users.noreply.github.com>
2020-09-07 20:21:09 +0800

    Fix Layout
    Fix Layout

--- subpages/about.js
+++ subpages/about.js
@@ -2,3 +2,8 @@ if (screen.width <= 900){
 	document.querySelector("#phone_card").style.margin = " 10% 0%"
 	document.querySelector("#text_area").style.height = "70vh"
 }
+
+const date = new Date();
+const system_time = date.getHours() + ":" + date.getMinutes()
+
+document.querySelector("#current_time").innerHTML = system_time
