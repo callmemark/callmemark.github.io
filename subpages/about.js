2eef1bd26a10d906ae488af5c812c7ff9c35e2e5
Mark John A. Velmonte <65215676+callmemark@users.noreply.github.com>
2020-09-08 18:26:47 +0800

    New About Page
    New About Page

--- subpages/about.js
+++ subpages/about.js
@@ -1,9 +1,1 @@
-if (screen.width <= 900){
-	document.querySelector("#phone_card").style.margin = " 10% 0%"
-	document.querySelector("#text_area").style.height = "70vh"
-}
-
-const date = new Date();
-const system_time = date.getHours() + ":" + date.getMinutes()
-
-document.querySelector("#current_time").innerHTML = system_time
+console.log("loaded")
