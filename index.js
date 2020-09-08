884e32c249594fb26e5bd0b4d571becb29f821c1
Mark John A. Velmonte <65215676+callmemark@users.noreply.github.com>
2020-09-08 18:25:44 +0800

    Added loading screen
    Added loading screen

--- index.js
+++ index.js
@@ -1,3 +1,90 @@
+const loader = document.querySelector("#loader")
+
+
+const letters = [
+	document.querySelector("#letter_l").innerHTML,
+	document.querySelector("#letter_o").innerHTML,
+	document.querySelector("#letter_a").innerHTML,
+	document.querySelector("#letter_d").innerHTML,
+	document.querySelector("#letter_i").innerHTML,
+	document.querySelector("#letter_n").innerHTML,
+	document.querySelector("#letter_n").innerHTML
+	]
+
+const loading_anim = ()=>{
+	anime({
+	  targets: "#letter_l",
+	  translateY: -50,
+	  direction: 'alternate',
+	  delay: 100,
+	  loop: true,
+	  easing: 'easeInOutSine'
+	});
+
+	anime({
+	  targets: "#letter_o",
+	  translateY: -50,
+	  direction: 'alternate',
+	  delay: 150,
+	  loop: true,
+	  easing: 'easeInOutSine'
+	});
+	anime({
+	  targets: "#letter_a",
+	  translateY: -50,
+	  direction: 'alternate',
+	  delay: 200,
+	  loop: true,
+	  easing: 'easeInOutSine'
+	});
+	anime({
+	  targets: "#letter_d",
+	  translateY: -50,
+	  direction: 'alternate',
+	  delay: 250,
+	  loop: true,
+	  easing: 'easeInOutSine'
+	});
+	anime({
+	  targets: "#letter_i",
+	  translateY: -50,
+	  direction: 'alternate',
+	  delay: 300,
+	  loop: true,
+	  easing: 'easeInOutSine'
+	});
+	anime({
+	  targets: "#letter_n",
+	  translateY: -50,
+	  direction: 'alternate',
+	  delay: 350,
+	  loop: true,
+	  easing: 'easeInOutSine'
+	});
+	anime({
+	  targets: "#letter_g",
+	  translateY: -50,
+	  direction: 'alternate',
+	  delay: 400,
+	  loop: true,
+	  easing: 'easeInOutSine'
+	});
+}
+
+
+loading_anim()
+
+const loader_off = ()=>{
+	loader.style.display = "none"	
+}
+
+const loading_func = ()=>{
+	loader_off()
+	loading_anim()
+}
+
+
+
 new WOW().init();
 
 
@@ -33,7 +120,7 @@ const openPage = (element, link)=>{
 
 const stand_by = null
 
-openPage(about, stand_by)
+//openPage(about, "#about_title")
 openPage(contact, stand_by)
 openPage(repo, "https://github.com/callmemark")
 openPage(aiWebsite, "http://projectai.epizy.com/")
@@ -211,3 +298,4 @@ const webdev_card_reveal = new WOW({
 
 reveal.init();
 webdev_card_reveal.init()
+
